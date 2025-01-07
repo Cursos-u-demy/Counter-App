87ab2ea834ccffeadf8e2de6c1434212fd233d73
danti96 <edisson.alvarado205@gmail.com>
2025-01-06 22:52:46 -0500

    Agregando Iconos Globales

--- App.tsx
+++ App.tsx
@@ -2,13 +2,22 @@ import { SafeAreaView } from "react-native"
 import { HelloWordScreen } from './src/presentation/screens/HelloWorldScreen';
 import { CounterScreen } from "./src/presentation/screens/CounterScreen";
 import { PaperProvider } from "react-native-paper";
+import { CounterM3Screen } from './src/presentation/screens/CounterM3Screen';
+
+
+import IonIcon from 'react-native-vector-icons/Ionicons';
 
 export const App = ()=>{
   return (
-    <PaperProvider>
+    <PaperProvider
+      settings={{ 
+        icon:(props)=> <IonIcon {...props}/> 
+     }}
+    >
       <SafeAreaView style={{ flex: 1 }}>
         {/* <HelloWordScreen name='Edhisson Alvarado'/> */}
-        <CounterScreen/>
+        {/* <CounterScreen/> */}
+        <CounterM3Screen/>
       </SafeAreaView>
     </PaperProvider>
   )
